d5cb672a2873021706a87c67d3c8f92c2f86bd21
Marc Sabatella <marc@outsideshore.com>
2023-08-15 11:58:29 -0600

    add braille ASCII support

--- popup.js
+++ popup.js
@@ -1,21 +1,49 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const convertButton = document.getElementById("convertButton");
+
+  const convertDotsButton = document.getElementById("convertDotsButton");
+  const convertAsciiButton = document.getElementById("convertAsciiButton");
   const brailleInput = document.getElementById("brailleInput");
   const resultElement = document.getElementById("result");
 
-  convertButton.addEventListener("click", function () {
+  convertDotsButton.addEventListener("click", function () {
     const input = brailleInput.value.trim();
-    const inputLines = input.split("\n"); // Split input into lines
+    const inputLines = input.split("\n");
   
     let unicodeBraille = "";
     for (const line of inputLines) {
       const cellPatterns = convertPatternToBraille(line);
-      const unicodeLine = cellPatterns.split(" ").map(generate_braille_unicode).join(" ");
+      const unicodeLine = cellPatterns.split(" ").map(generate_braille_unicode).join("");
       unicodeBraille += unicodeLine + "\n";
     }
-  
-    resultElement.value = unicodeBraille; // Use innerHTML to interpret <br> elements
+    resultElement.value = unicodeBraille;
+    resultElement.focus();
   });
+
+  convertAsciiButton.addEventListener("click", function () {
+    const input = brailleInput.value;
+    const inputLines = input.split("\n"); // Split input into lines
+    let unicodeBraille = "";
+    for (const line of inputLines) {
+      const unicodeLine = convertAsciiToUnicode(line);
+      unicodeBraille += unicodeLine + "\n";
+    }
+    resultElement.value = unicodeBraille;
+    resultElement.focus();
+  });
+
+  function convertAsciiToUnicode(inputPattern) {
+    const asciiBraille = " A1B'K2L@CIF/MSP\"E3H9O6R^DJG>NTQ,*5<-U8V.%[$+X!&;:4\\0Z7(_?W]#Y)=";
+    let cellPattern = "";
+    for (const char of inputPattern) {
+      const index = asciiBraille.indexOf(char);
+      if (index !== -1) {
+        cellPattern += String.fromCharCode(0x2800 + index);
+      } else {
+        cellPattern += ""; // Handle unsupported characters
+      }
+    }
+    return cellPattern;
+  }
 
   function generate_braille_unicode(braille_pattern) {
     // Braille Unicode starts from U+2800
@@ -29,7 +57,6 @@ document.addEventListener("DOMContentLoaded", function () {
         braille_unicode += Math.pow(2, i);
       }
     }
-  
     return String.fromCodePoint(braille_unicode);
   }
 
@@ -43,7 +70,6 @@ document.addEventListener("DOMContentLoaded", function () {
       'l': '6',
       ';': '0'
     };
-  
     const numericString = sixKeyString.split('').map(char => perkinsToNumeric[char] || char).join('');
     return numericString;
   }
@@ -51,19 +77,16 @@ document.addEventListener("DOMContentLoaded", function () {
   function convertPatternToBraille(inputPattern) {
     const patternArr = inputPattern.split(" ");
     let braillePattern = "";
-  
     for (const pattern of patternArr) {
       const numericPattern = convertSixKeyToNumeric(pattern);
       const digits = numericPattern.split("");
       let cellPattern = "";
-  
       for (let i = 0; i < 6; i++) {
         cellPattern += digits.includes((i + 1).toString()) ? "1" : "0";
       }
-  
       braillePattern += cellPattern + " ";
     }
-  
-    return braillePattern.trim(); // Remove trailing space
+    return braillePattern.trim();
   }
+  
 });
